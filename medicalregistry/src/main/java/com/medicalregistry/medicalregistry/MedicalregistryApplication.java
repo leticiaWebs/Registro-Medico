0aa0cd92392b499d8fd6968b2a15cd216793dda3
leticiaWebs <leticiaaod@gmail.com>
2023-12-05 16:54:11 -0300

    Projeto criado

---
+++ medicalregistry/src/main/java/com/medicalregistry/medicalregistry/MedicalregistryApplication.java
@@ -0,0 +1,13 @@
+package com.medicalregistry.medicalregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalregistryApplication.class, args);
+	}
+
+}
---
+++ medicalregistry/src/test/java/com/medicalregistry/medicalregistry/MedicalregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalregistry.medicalregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
